be998acfc2a60e3f3f55c80dbda1f64e84d95096
Jonathan Saccomano <50148287+MESTRESCII@users.noreply.github.com>
2022-04-22 16:03:22 -0300

    Adicionando UI

---
+++ gameUI.py
@@ -0,0 +1,12 @@
+from game import GameTimer
+import tkinter
+from tkinter import messagebox
+
+
+class TelaDoJogo(object):
+    def __init__(self):
+
+        self.root = tkinter.Tk()
+        self.root.geometry("300x250")
+
+        self.root.mainloop()
