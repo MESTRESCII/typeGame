94ea1bb06392d586366c9983b931e6a32f99c459
Abhner27 <abhner.saccomano@hotmail.com>
2022-04-22 16:43:39 -0300

    Adicionei as primeiras funcionalidades ao typer e a geração/escolha de frases

--- sentencesMap.py
+++ sentencesMap.py
@@ -1,5 +1,42 @@
+import click as clk
+from numpy import char
+
 dicionarioDeFrases = dict(
     {1: "Oi mundo",   2: "Esse é um typeGame feito em python"})
 
-print(dicionarioDeFrases[1])
-print(dicionarioDeFrases[2])
+
+class GeradorDeFrases(object):
+    index = 0
+
+    def __init__(self, index:int) -> None:
+        index = self.index
+        pass
+        
+    def gerarFrase(self, index: int) -> str:
+        self.index = index
+        return dicionarioDeFrases[index]
+
+    def pegarFrase(self) -> str:
+        return dicionarioDeFrases[self.index]
+
+    def retornarCharDoIndex(self, index:int) -> char:
+        return dicionarioDeFrases[self.index][index]
+
+        
+
+
+gerador = GeradorDeFrases(1)
+minhaResposta = print("Escreva a seguinte frase: {} ".format(gerador.gerarFrase(1)))
+
+indexDeChar = 0
+
+while(indexDeChar < len(gerador.pegarFrase())):
+    c = clk.getchar()   # Gets a single character
+
+    if c == gerador.retornarCharDoIndex(indexDeChar):
+        indexDeChar+=1
+        print(c)
+    else: #errou
+        break
+
+
